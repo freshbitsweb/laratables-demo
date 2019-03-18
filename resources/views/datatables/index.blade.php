0c4d44a3a74a95cf7f0af2e54c41c2c0daed18aa
Ramesh <ramesh@freshbits.in>
2019-03-18 16:53:44 +0530

    Remove other Code

--- resources/views/datatables/index.blade.php
+++ resources/views/datatables/index.blade.php
@@ -12,54 +12,6 @@
                 <div class="card-body">
                     <div class="accordion" id="accordionExample">
                         <div class="card">
-                            <div class="card-header bg-dark" id="simple-heading">
-                                <h5 class="mb-0">
-                                    <button class="btn btn-link text-white" type="button" data-toggle="collapse" data-target="#simple" aria-expanded="true" aria-controls="simple">
-                                        <h4 class="m-0">Simple</h4>
-                                    </button>
-                                </h5>
-                            </div>
-
-                            <div id="simple" class="collapse" aria-labelledby="simple-heading" data-parent="#accordionExample">
-                                <div class="card-body">
-                                    @include('datatables.includes.simple')
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="card">
-                            <div class="card-header bg-dark" id="relationship-column-heading">
-                                <h5 class="mb-0">
-                                    <button class="btn btn-link text-white" type="button" data-toggle="collapse" data-target="#relationship-column" aria-expanded="true" aria-controls="relationship-column">
-                                        <h4 class="m-0">Relationship Columns</h4>
-                                    </button>
-                                </h5>
-                            </div>
-
-                            <div id="relationship-column" class="collapse" aria-labelledby="relationship-column-heading" data-parent="#accordionExample">
-                                <div class="card-body">
-                                    @include('datatables.includes.relationship_column')
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="card">
-                            <div class="card-header bg-dark" id="extra-data-datatables-attribute-heading">
-                                <h5 class="mb-0">
-                                    <button class="btn btn-link text-white" type="button" data-toggle="collapse" data-target="#extra-data-datatables-attribute" aria-expanded="true" aria-controls="extra-data-datatables-attribute">
-                                        <h4 class="m-0">Extra Data Datatables Attributes</h4>
-                                    </button>
-                                </h5>
-                            </div>
-
-                            <div id="extra-data-datatables-attribute" class="collapse" aria-labelledby="extra-data-datatables-attribute-heading" data-parent="#accordionExample">
-                                <div class="card-body">
-                                    @include('datatables.includes.extra_data_datatables_attributes')
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="card">
                             <div class="card-header bg-dark" id="controlling-query">
                                 <h5 class="mb-0">
                                     <button class="btn btn-link text-white" type="button" data-toggle="collapse" data-target="#query-controlling" aria-expanded="true" aria-controls="query-controlling">
