69b5eee91076137da4771e57a3238fd1195a4a6b
Ramesh <ramesh@freshbits.in>
2019-03-18 17:40:34 +0530

    make separate file

---
+++ resources/views/datatables/controlling_the_query.blade.php
@@ -0,0 +1,15 @@
+<div class="card">
+    <div class="card-header bg-dark" id="controlling-query">
+        <h5 class="mb-0">
+            <button class="btn btn-link text-white" type="button" data-toggle="collapse" data-target="#query-controlling" aria-expanded="true" aria-controls="query-controlling">
+                <h4 class="m-0">Controlling the query</h4>
+            </button>
+        </h5>
+    </div>
+
+    <div id="query-controlling" class="collapse" aria-labelledby="controlling-query" data-parent="#accordionExample">
+        <div class="card-body">
+            @include('datatables.includes.controlling_the_query')
+        </div>
+    </div>
+</div>
--- resources/views/datatables/index.blade.php
+++ resources/views/datatables/index.blade.php
@@ -11,21 +11,8 @@
 
                 <div class="card-body">
                     <div class="accordion" id="accordionExample">
-                        <div class="card">
-                            <div class="card-header bg-dark" id="controlling-query">
-                                <h5 class="mb-0">
-                                    <button class="btn btn-link text-white" type="button" data-toggle="collapse" data-target="#query-controlling" aria-expanded="true" aria-controls="query-controlling">
-                                        <h4 class="m-0">Controlling the query</h4>
-                                    </button>
-                                </h5>
-                            </div>
 
-                            <div id="query-controlling" class="collapse" aria-labelledby="controlling-query" data-parent="#accordionExample">
-                                <div class="card-body">
-                                    @include('datatables.includes.controlling_the_query')
-                                </div>
-                            </div>
-                        </div>
+                        @include('datatables.controlling_the_query')
 
                         <div class="card">
                             <div class="card-header bg-dark" id="custom-column-heading">
