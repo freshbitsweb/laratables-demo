3f7891583466415a0e5a9422064b5080eea028fb
Ramesh <ramesh@freshbits.in>
2019-03-19 16:01:59 +0530

    Remove Unused Codec

--- app/Http/Controllers/DataTableController.php
+++ app/Http/Controllers/DataTableController.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
-use App\Product;
-use Freshbitsweb\Laratables\Laratables;
-
 class DataTableController extends Controller
 {
     /**
@@ -16,35 +12,5 @@ class DataTableController extends Controller
     public function index()
     {
         return view('datatables.index');
-    }
-
-    /**
-     * return data of the simple datatables.
-     *
-     * @return Json
-     */
-    public function getSimpleDatatablesData()
-    {
-        return Laratables::recordsOf(User::class);
-    }
-
-    /**
-     * return data of the relation columns datatables.
-     *
-     * @return Json
-     */
-    public function getRelationshipColumnDatatablesData()
-    {
-        return Laratables::recordsOf(Product::class);
-    }
-
-    /**
-     * return data of the Extra data datatables attribute data.
-     *
-     * @return Json
-     */
-    public function getExtraDataDatatablesAttributesData()
-    {
-        return Laratables::recordsOf(User::class);
     }
 }
